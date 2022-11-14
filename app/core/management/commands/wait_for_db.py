9ad70f948dfbc698b7d88e71cf600463861f7bfa
jpolanco <polanquillo2@gmail.com>
2022-11-14 15:55:56 -0600

    add wait for db for django

---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,23 @@
+"""
+Django command to wait for the DB is available.
+"""
+import time
+
+from psycopg2 import OperationalError as Psycopg2opError
+from django.db.utils import OperationalError
+from django.core.management.base import BaseCommand
+
+class Command(BaseCommand):
+    
+    
+    def handle(self, *args, **options):
+        self.stdout.write("Waiting for database...")
+        db_up = False
+        while db_up is False:
+            try:
+                self.check(databases=['default'])
+                db_up = True
+            except(Psycopg2opError, OperationalError):
+                self.stdout.write('Database is unavailable, waiting 1 second')
+                time.sleep(1)
+        self.stdout.write(self.style.SUCCESS("Database is ready!"))
