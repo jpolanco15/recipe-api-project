fec7a28703e3d63fb10af435ec118d9ea5f5d38a
jpolanco <polanquillo2@gmail.com>
2023-01-05 14:44:05 -0600

    user functionallity created

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -3,7 +3,7 @@ from django.contrib.auth import get_user_model
 from django.urls import reverse
 
 from rest_framework.test import APIClient
-from rest_framework.test import status
+from rest_framework import status
 
 
 CREATE_USER_URL = reverse('user:create')
